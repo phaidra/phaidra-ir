ea472f0fcffea8e42963918d97d61dcc0a7d8e62
Sandeep Chandupatla <sandeepc90@univie.ac.at>
2023-11-30 17:07:18 +0530

    datacite implementation

--- components/AdminSubmitIr.vue
+++ components/AdminSubmitIr.vue
@@ -1262,7 +1262,156 @@ export default {
     },
     searchForDoiRecord: async function () {
       try {
+        let doiAgency = null
+        // return
         this.loading = true
+        try {
+          const agencyResp = await axios.get(`https://api.crossref.org/works/${this.doiSearchInput}/agency`)
+          doiAgency = agencyResp?.data?.message?.agency?.id
+        } catch (error) {
+          console.log('Doi agency error', error)
+        }
+        console.log('doiAgency', doiAgency)
+        if(doiAgency === 'datacite'){
+          this.loading = false
+          this.showDoiSearchTable = false;
+          this.doiImportData = {
+              // doi: this.doiToImport.replace(/\s\s+/g, ' ').trim(),
+              title: '',
+              dateIssued: '',
+              authors: [],
+              publicationType: '',
+              publisher: '',
+              journalTitle: '',
+              journalISSN: '',
+              journalVolume: '',
+              journalIssue: '',
+              pageStart: '',
+              pageEnd: '',
+              licenceLabel: ''
+            }
+            const dataciteResp = await axios.get(`https://api.datacite.org/dois/${this.doiSearchInput}`)
+            const dataciteData = dataciteResp?.data;
+            if(dataciteData?.data?.attributes?.titles?.length){
+              this.doiImportData.title = dataciteData.data.attributes.titles[0].title
+            }
+            if(dataciteData?.data?.attributes?.publicationYear){
+              this.doiImportData.dateIssued = dataciteData?.data?.attributes?.publicationYear
+            }
+            if(dataciteData?.data?.attributes?.creators){
+              let authors = dataciteData?.data?.attributes?.creators
+              if (authors.length > 0) {
+                for (let author of authors) {
+                  if (author['givenName'] || author['familyName']) {
+                    let auth = {
+                      type: 'schema:Person',
+                      firstname: author['givenName'] ? author['givenName'].replace(/\s\s+/g, ' ').trim() : '',
+                      lastname: author['familyName'] ? author['familyName'].replace(/\s\s+/g, ' ').trim() : ''
+                    }
+                    if (author['affiliation']) {
+                      if (Array.isArray(author['affiliation'])) {
+                        auth.affiliation = []
+                        for (let af of author['affiliation']) {
+                          auth.affiliation.push(af['name'])
+                        }
+                      }
+                    }
+                    if (author['ORCID']) {
+                      auth.orcid = author['ORCID'].replace('http://orcid.org/', '')
+                    }
+                    this.doiImportData.authors.push(auth)
+                  }
+                  if (author['name']) {
+                    let auth = {
+                      type: 'schema:Organization',
+                      name: author['name']
+                    }
+                    this.doiImportData.authors.push(auth)
+                  }
+                }
+              }
+            }
+            if (dataciteData?.data?.attributes?.language) {
+              if (this.lang2to3map[dataciteData.data.attributes.language]) {
+                this.doiImportData.language = this.lang2to3map[dataciteData.data.attributes.language]
+              }
+            }
+            if(dataciteData?.data?.attributes?.types?.citeproc){
+              const dataciteType = dataciteData.data.attributes.types.citeproc;
+              switch (dataciteType) {
+                case 'article':
+                case 'journal-article':
+                case 'article-journal':
+                  this.doiImportData.publicationType = 'article'
+                  this.doiImportData.publicationTypeId = 'https://pid.phaidra.org/vocabulary/VKA6-9XTY'
+                  break
+                case 'report':
+                  this.doiImportData.publicationType = 'report'
+                  this.doiImportData.publicationTypeId = 'https://pid.phaidra.org/vocabulary/JMAV-7F3R'
+                  break
+                case 'book':
+                case 'monograph':
+                case 'reference-book':
+                case 'edited-book':
+                  this.doiImportData.publicationType = 'book'
+                  this.doiImportData.publicationTypeId = 'https://pid.phaidra.org/vocabulary/47QB-8QF1'
+                  break
+                case 'book-chapter':
+                case 'book-part':
+                case 'book-section':
+                  this.doiImportData.publicationType = 'book_part'
+                  this.doiImportData.publicationTypeId = 'https://pid.phaidra.org/vocabulary/XA52-09WA'
+                  break
+                case 'dissertation':
+                  this.doiImportData.publicationType = 'doctoral_thesis'
+                  this.doiImportData.publicationTypeId = 'https://pid.phaidra.org/vocabulary/1PHE-7VMS'
+                  break
+                case 'proceedings-article':
+                case 'proceedings':
+                  this.doiImportData.publicationType = 'conference_object'
+                  this.doiImportData.publicationTypeId = 'https://pid.phaidra.org/vocabulary/QKDF-E5HA'
+                  break
+                case 'dataset':
+                  this.doiImportData.publicationType = 'research_data'
+                  this.doiImportData.publicationTypeId = 'https://pid.phaidra.org/vocabulary/KW6N-2VTP'
+                  break
+                case 'other':
+                case 'standard':
+                case 'standard-series':
+                case 'book-entry':
+                case 'book-series':
+                case 'book-set':
+                case 'book-track':
+                case 'component':
+                case 'journal-issue':
+                case 'journal-volume':
+                case 'journal':
+                case 'report-series':
+                  this.doiImportData.publicationType = 'other'
+                  this.doiImportData.publicationTypeId = 'https://pid.phaidra.org/vocabulary/PYRE-RAWJ'
+                  break
+                default:
+                  this.doiImportData.publicationType = 'other'
+                  this.doiImportData.publicationTypeId = 'https://pid.phaidra.org/vocabulary/PYRE-RAWJ'
+              }
+            }
+            if(dataciteData?.data?.attributes?.publisher){
+              this.doiImportData.publisher = this.$_.unescape(dataciteData.data.attributes.publisher.replace(/\s\s+/g, ' ').trim())
+            }
+            if (dataciteData?.data?.attributes?.subjects) {
+              if (Array.isArray(dataciteData.data.attributes.subjects)) {
+                this.doiImportData.keywords = []
+                for (let kw of dataciteData.data.attributes.subjects) {
+                  this.doiImportData.keywords.push(kw)
+                }
+              }
+            }
+            if(dataciteData?.data?.attributes?.container?.title) {
+              this.doiImportData.journalTitle = this.$_.unescape(dataciteData?.data?.attributes?.container?.title.replace(/\s\s+/g, ' ').trim())
+            }
+          // this.resetForm(this, this.doiImportData)
+          return
+        }
         let response = await axios.get(`https://api.crossref.org/works?rows=5&offset=0&query=${this.doiSearchInput}`)
         this.loading = false
         if(response?.data?.message?.items){
